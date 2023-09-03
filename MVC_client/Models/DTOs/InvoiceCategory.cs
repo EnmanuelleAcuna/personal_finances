be8b6a879b1635a8dd28cf5909ed7fb10afe5141
Enmanuelle Acuña <emanuelacu@outlook.com>
2023-09-03 09:20:16 -0600

    Delete old MVC project and update .sln file to remove reference to this MVC.

--- MVC_client/Models/DTOs/InvoiceCategory.cs
+++
@@ -1,7 +0,0 @@
-namespace MVC_Client.Models;
-
-public class InvoiceCategory
-{
-	public Guid Id { get; set; }
-	public string? Name { get; set; }
-}
--- MVC_client/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MVC_Client.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
